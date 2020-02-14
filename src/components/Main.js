7dc350d7fb8149b9c9d5ba61754d44e9ef7a81a4
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-14 07:54:47 +0100

    setup initial project layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,48 @@
 import React from "react";
 import { Typography, PageHeader } from "antd";
+import { Layout } from "antd";
 
+const { Header, Content, Footer } = Layout;
 const { Text } = Typography;
 
 export default function Main() {
+  const today = new Date();
   return (
     <>
-      <header>
-        <PageHeader
-          style={{
-            border: "1px solid rgb(235, 237, 240)"
-          }}
-          onBack={() => null}
-          title="Products Grid"
-          subTitle="Here you're sure to find a bargain on some of the finest ascii
+      <Layout className="layout">
+        <Header>
+          <div className="logo">
+            <img
+              src="https://creatella.ventures/wp-content/uploads/2016/03/creatella-logo-2x.png"
+              alt="creatella logo"
+            />
+          </div>
+        </Header>
+        <Content>
+          <PageHeader
+            title="Products Grid"
+            subTitle="Here you're sure to find a bargain on some of the finest ascii
           available to purchase. Be sure to peruse our selection of ascii faces
           in an exciting range of sizes and prices."
-        />
-        <Text strong>But first, a word from our sponsors:</Text>
-        <br />
-        <img
-          className="ad"
-          src={
-            "http://localhost:3000/ads/?r=" +
-            Math.floor(Math.random() * 1000) +
-            ""
-          }
-        />
-      </header>
-
-      <section className="products">... products go here ...</section>
+          />
+          <div>
+            <Text strong>But first, a word from our sponsors:</Text>
+            <br />
+            <img
+              className="ad"
+              src={
+                "http://localhost:3000/ads/?r=" +
+                Math.floor(Math.random() * 1000) +
+                ""
+              }
+            />
+            <section className="products">... products go here ...</section>
+          </div>
+        </Content>
+        <Footer>
+          Creatella ©{today.getFullYear()} Created by Yusuf Abdulkarim
+        </Footer>
+      </Layout>
     </>
   );
 }
