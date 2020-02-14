2a3faeed705c8c7a50e9a20ef61050bf5e997822
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-14 06:05:42 +0100

    add support for ant design system This is mainly for consistent styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,31 @@
 import React from "react";
+import { Typography, Divider } from "antd";
+
+const { Title, Paragraph, Text } = Typography;
 
 export default function Main() {
   return (
     <>
       <header>
-        <h1>Products Grid</h1>
-        <p>
+        <Title>Products Grid</Title>
+        <Paragraph>
           Here you're sure to find a bargain on some of the finest ascii
           available to purchase. Be sure to peruse our selection of ascii faces
           in an exciting range of sizes and prices.
-        </p>
-        <p>But first, a word from our sponsors:</p>{" "}
-        
-          <img
-            class="ad"
-            src={"http://localhost:3000/ads/?r=" + Math.floor(Math.random()*1000) + ""}
-          />
+        </Paragraph>
+        <Text strong>But first, a word from our sponsors:</Text>
+        <Divider />
+        <img
+          className="ad"
+          src={
+            "http://localhost:3000/ads/?r=" +
+            Math.floor(Math.random() * 1000) +
+            ""
+          }
+        />
       </header>
 
-      <section class="products">... products go here ...</section>
+      <section className="products">... products go here ...</section>
     </>
   );
 }
