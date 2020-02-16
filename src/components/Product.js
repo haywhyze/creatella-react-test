2d6d06a75c61c2fbce5b824fa9b62bc722dec8c8
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-16 17:38:39 +0100

    create product card

---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Statistic, Tag } from "antd";
+import {formatPrice, formatDate} from "../helpers";
+
+export default function Product({ product }) {
+  return (
+    <div className={"face-card"}>
+      <span className="face" style={{ fontSize: product.size }}>{product.face}</span>
+      <Statistic value={formatPrice(product.price)} />
+      <Tag color="#001529">{formatDate(product.date)}</Tag>
+      </div>
+  );
+}
