deadb5d8b4e32e66e1f4414fe3b70aa87ff387ff
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-17 11:31:28 +0100

    format product card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Statistic, Tag } from "antd";
-import {formatPrice, formatDate} from "../helpers";
+import { formatPrice, formatDate } from "../helpers";
 
 export default function Product({ product }) {
   return (
     <div className={"face-card"}>
-      <span className="face" style={{ fontSize: product.size }}>{product.face}</span>
+      <span className="face" style={{ fontSize: product.size }}>
+        {product.face}
+      </span>
       <Statistic value={formatPrice(product.price)} />
       <Tag color="#001529">{formatDate(product.date)}</Tag>
-      </div>
+    </div>
   );
 }
