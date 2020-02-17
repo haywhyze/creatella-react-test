a2d2d5bee71ac09ef715e73c7270d727f3f933b3
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-17 10:38:41 +0100

    add ternary expression to api call to handle sort

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import Axios from "axios";
 
 export const baseUrl = "http://localhost:3000/";
 
-export const fetchData = (success, failure, page, limit) => {
-  Axios.get(`${baseUrl}api/products?_page=${page}&_limit=${limit}`)
+export const fetchData = (success, failure, page, limit, sort) => {
+  Axios.get(`${baseUrl}api/products?${sort ? "_sort=" + sort+"&" : ""}_page=${page}&_limit=${limit}`)
     .then(res => success(res))
     .catch(error => failure(error))
   ;
